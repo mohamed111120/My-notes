fc079ca72c33038ee9abd330abd26ab896f57b7c
Mohamed Osama <memeususu111@gmail.com>
2023-05-04 20:14:34 +0300

    Ch 14,15 vandad nahavandipoor

---
+++ lib/views/login_page.dart
@@ -0,0 +1,92 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import '../firebase_options.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+
+class _LoginPageState extends State<LoginPage> {
+  late TextEditingController _email;
+  late TextEditingController _password;
+
+  @override
+  void initState() {
+    
+    _email =TextEditingController();
+    _password =TextEditingController();
+  
+      super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: FutureBuilder(
+        future:  Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+        ) ,
+        builder: (context, snapshot) {
+
+          switch (snapshot.connectionState) {
+            
+            case ConnectionState.done:
+              return Column(
+          children: [
+            TextField(
+              enableSuggestions: false,
+              autocorrect:false ,
+              keyboardType:TextInputType.emailAddress ,
+              controller: _email,
+              decoration: InputDecoration(hintText: 'Enter your e-mail'),
+            ),
+            TextField(
+              enableSuggestions: false,
+              autocorrect:false ,
+                obscureText: true,
+                controller: _password,
+              decoration: InputDecoration(hintText: 'Enter your password'),
+            ),
+            Center(
+              child: TextButton(
+                child: Text('Login'),
+                onPressed: () async {
+      
+                  
+      
+                  final email = _email.text;
+                  final password = _password.text;
+      
+                final UserCredential= await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: email, password: password);
+                      print(UserCredential);
+                },
+              ),
+            ),
+          ],
+        );
+
+        default : return Text('Lofing....');
+          }
+
+           
+        },
+      ),
+    );
+  }
+}
